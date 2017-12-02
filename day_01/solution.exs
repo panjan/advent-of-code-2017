d5e0ecc4fde3137b9aae308094bc4b042d1dc98d
panjan <jan.zdrahal@gooddata.com>
2017-12-02 14:02:19 +0100

    Beautify

--- day_01/solution.exs
+++ day_01/solution.exs
@@ -1,20 +1,14 @@
-
 defmodule Captcha do
   def solve(list, rotation) do
     {first_half, second_half} = Enum.split(list, rotation)
     rotated_list = second_half ++ first_half
     [list, rotated_list]
     |> Enum.zip
-    |> Enum.filter(fn({x,y}) -> x == y end)
-    |> Enum.map(fn({x,_}) -> x end)
+    |> Enum.filter(fn({x, y}) -> x == y end)
+    |> Enum.map(fn({x, _}) -> x end)
     |> Enum.sum
   end
-  def first_part(list) do
-    Captcha.solve(list, 1)
-  end
-  def second_part(list) do
-    Captcha.solve(list, div(length(list),2))
-  end
+
   def parse_input do
     {:ok, input} = File.read("day_01/input.txt")
     input
@@ -24,6 +18,10 @@ defmodule Captcha do
   end
 end
 
-input = Captcha.parse_input()
-IO.puts("First part: #{Captcha.first_part(input)}")
-IO.puts("Second part: #{Captcha.second_part(input)}")
+numbers = Captcha.parse_input()
+first_part = Captcha.solve(numbers, 1)
+IO.puts("First part: #{first_part}")
+
+half = div(length(numbers), 2)
+second_part = Captcha.solve(numbers, half)
+IO.puts("Second part: #{second_part}")
