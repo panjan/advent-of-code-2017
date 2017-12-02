8818254bf8aa31173df216b45d73a9d91e95c94e
panjan <jan.zdrahal@gooddata.com>
2017-12-02 11:40:29 +0100

    DRY up

--- day_01/solution.exs
+++ day_01/solution.exs
@@ -1,17 +1,24 @@
-{:ok, input} = File.read("day_01/input.txt")
 
 defmodule Captcha do
   def solve(_prev, []), do: 0
   def solve(prev, [head|tail]) do
     if prev == head, do: prev + solve(head, tail), else: solve(head, tail)
   end
+  def first_part do
+    input_ints = parse_input()
+    circular_list = input_ints ++ [List.first(input_ints)]
+    Captcha.solve(0, circular_list)
+  end
+  def second_part do
+
+  end
+  def parse_input do
+    {:ok, input} = File.read("day_01/input.txt")
+    input
+    |> String.trim()
+    |> String.graphemes()
+    |> Enum.map(&String.to_integer(&1))
+  end
 end
 
-input_ints = input
-             |> String.trim()
-             |> String.graphemes()
-             |> Enum.map(&String.to_integer(&1))
-
-circular_list = input_ints ++ [List.first(input_ints)]
-
-IO.puts(Captcha.solve(0, circular_list))
+IO.puts(Captcha.first_part)
