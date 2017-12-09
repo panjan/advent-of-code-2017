6f164d70e7f030c1fb1e5424105c74bc3a194ce2
panjan <jan.zdrahal@gooddata.com>
2017-12-09 12:06:42 +0100

    Refactor part 1

--- day_03/solution.exs
+++ day_03/solution.exs
@@ -3,25 +3,19 @@ defmodule Spiral do
     cond do
       number == 1
         -> abs(x) + abs(y)
-      x == size - 1 && y == 1 - size
+      x == size && y == -size
         -> solve(number - 1, size + 1, {x+1,y})
-      x == size - 1 && y == size - 1
-        -> solve(number - 1, size, {x-1,y})
-      x == 1 - size && y == size - 1
-        -> solve(number - 1, size, {x,y-1})
-      x == 1 - size && y == 1 - size
+      y == -size
         -> solve(number - 1, size, {x+1,y})
-      y == 1 - size
-        -> solve(number - 1, size, {x+1,y})
-      x == 1 - size
+      x == -size
         -> solve(number - 1, size, {x,y-1})
-      y == size - 1
+      y == size
         -> solve(number - 1, size, {x-1,y})
-      x == size - 1
+      x == size
         -> solve(number - 1, size, {x,y+1})
     end
   end
 end
 IO.puts("First part:")
-IO.inspect(Spiral.solve(277678, 1, {0, 0}))
+IO.inspect(Spiral.solve(277678, 0, {0, 0}))
 IO.puts("Second part:")
