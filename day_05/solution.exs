3594b933bde25529763e0ee815d4c4e48779941a
panjan <jan.zdrahal@gooddata.com>
2017-12-26 23:49:00 +0100

    Solve day 5 - part 1

--- day_05/solution.exs
+++ day_05/solution.exs
@@ -1,6 +1,16 @@
 defmodule Maze do
   def solve_part_1(maze) do
-    maze
+    step(maze, 0, 0)
+  end
+
+  def step(maze, index, steps) do
+    if Enum.member?(0..Enum.count(maze) - 1, index) do
+      value = Enum.at(maze, index)
+      new_maze = maze |> List.replace_at(index, value + 1)
+      step(new_maze, index + value, steps + 1)
+    else
+      steps
+    end
   end
 
   def parse_input do
